2019b6753f8e5d07a608e931cc326d2a73e02a41
Praveen Kumar T <praveen.tri247@gmail.com>
2025-01-05 15:32:47 +0530

    Initial Commit

---
+++ ClimateAppSwiftUI/ClimateAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateAppSwiftUIApp.swift
+//  ClimateAppSwiftUI
+//
+//  Created by Talari Praveen kumar on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ClimateAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimateAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClimateAppSwiftUI
+//
+//  Created by Talari Praveen kumar on 05/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
